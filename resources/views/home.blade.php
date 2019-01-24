de34959034cd34210cc7d98a4e698139b7a2bfe5
jtibbetts1998 <justin.tibbetts98@gmail.com>
2019-01-24 14:26:57 -0500

    Frame work for home page complete

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -31,32 +31,32 @@
         </ol>
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <img class="first-slide" src="{{ asset('images/Mic02.jpg') }}" alt="First slide" width="100%">
+            <img class="first-slide" src="{{ asset('images/Mic02.jpg') }}" alt="First slide" height="600px" width="100%">
             <div class="container">
               <div class="carousel-caption text-left">
                 <h1>Listen</h1>
                 <p>Save content to your library, make playlists, and share</p>
-                <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+                <a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a>
               </div>
             </div>
           </div>
           <div class="carousel-item">
-            <img class="second-slide" src="{{ asset('images/Audio3.jpg') }}" alt="Second slide" width="100%">
+            <img class="second-slide" src="{{ asset('images/Audio3.jpg') }}" alt="Second slide" height="600px" width="100%">
             <div class="container">
               <div class="carousel-caption">
                 <h1>Create</h1>
                 <p>Record and upload your music, share with others in the community</p>
-                <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+                <a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a>
               </div>
             </div>
           </div>
           <div class="carousel-item">
-            <img class="third-slide" src="{{ asset('images/stockLaptop.jpg') }}" alt="Third slide" width="100%">
+            <img class="third-slide" src="{{ asset('images/stockLaptop.jpg') }}" alt="Third slide" height="600px" width="100%">
             <div class="container">
               <div class="carousel-caption text-right">
                 <h1>Discover</h1>
                 <p>Find out new and local artists</p>
-                <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+                <a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a>
               </div>
             </div>
           </div>
@@ -73,30 +73,30 @@
 
         <!-- START THE FEATURETTES -->
         <div class="container">
-
-        <hr class="featurette-divider">
+            
+        <br>
 
         <div class="row featurette">
           <div class="col-md-7">
             <h2 class="featurette-heading">Welcome to Nu-Music! <span class="text-muted">It'll blow your mind.</span></h2>
-            <p class="lead">Join today to recieve 10% off your first purchase, along with a subscription to our site.</p>
+            <p class="lead">Join today to recieve 10% off your first purchase, along with a subscription to our site. Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
             <a class="btn btn-outline-primary" href="#" role="button">Sign Up</a>
           </div>
           <div class="col-md-5">
-            <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+            <img class="featurette-image img-fluid mx-auto" src="{{ asset('images/soundBars.jpg') }}" alt="Generic placeholder image">
           </div>
         </div>
 
         <hr class="featurette-divider">
 
         <div class="row featurette">
-          <div class="col-md-7 order-md-2">
+          <div class="col-md-7 order-md-2" style="float: right;">
             <h2 class="featurette-heading">Shop. <span class="text-muted">Checkmate.</span></h2>
-            <p class="lead">Browse for Beats, Songs, or Albums</p>
+            <p class="lead">Browse for Beats, Songs, or Albums. Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
             <a class="btn btn-outline-primary" href="#" role="button">Begin Here</a>
           </div>
           <div class="col-md-5 order-md-1">
-            <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+            <img class="featurette-image img-fluid mx-auto" src="{{ asset('images/Music1.jpg') }}" alt="Generic placeholder image">
           </div>
         </div>
 
@@ -109,7 +109,7 @@
             <a class="btn btn-outline-primary" href="#" role="button">Learn More</a>
           </div>
           <div class="col-md-5">
-            <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+            <img class="featurette-image img-fluid mx-auto" src="{{ asset('images/Crowd1.jpg') }}" alt="Generic placeholder image">
           </div>
         </div>
 
